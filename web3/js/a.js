f30bac989e6b4badd5a561da69b0a63be6e0a521
gin-lsl <gin_lsl@outlook.com>
2017-06-24 18:34:33 +0800

    完成web3

--- web3/js/a.js
+++ web3/js/a.js
@@ -1,3 +1,16 @@
 (function () {
-    console.log('%c%s', 'background: yellow; color: red; font-size: 24px;', '我的世界')
+    console.log('%c%s', 'background: yellow; color: red; font-size: 24px;', '测试')
+    window.onload = function () {
+        var navbtns = document.getElementsByClassName('navbtn')
+        for (var i = 0; i < navbtns.length; i++) {
+            (function (i) {
+                var _id = navbtns[i].getAttribute('data-navbtn-toggle')
+                var _toggleTarget = document.getElementById(_id)
+                navbtns[i].onclick = function () {
+                    _toggleTarget.classList.toggle('open')
+                    navbtns[i].textContent = navbtns[i].textContent == '打开' ? '关闭' : '打开'
+                }
+            }(i))
+        }
+    }
 }())
